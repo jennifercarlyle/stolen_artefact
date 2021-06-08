10ddc0f3d3097050fcf0833e89300c6e24b0b335
Jennifer Carlyle <jennifercarlyle@gmail.com>
2021-06-08 16:08:06 +0100

    fix syntax error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @favorited_artefacts = @user.favorites.map do { |f| Artefact.find(f.artefact_id) }
+    @favorited_artefacts = @user.favorites.map { |f| Artefact.find(f.artefact_id) }
   end
 end
