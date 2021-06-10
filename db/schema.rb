aef6fc0cd0b86a07aeb901fea681e73ab76d32b3
Beverly Chan <beverlyc.uk@gmail.com>
2021-06-10 14:26:57 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_101404) do
+ActiveRecord::Schema.define(version: 2021_06_10_130317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_101404) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.date "date"
+    t.string "date"
     t.text "description"
     t.bigint "artefact_id", null: false
     t.datetime "created_at", precision: 6, null: false
