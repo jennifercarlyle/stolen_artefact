5b4b456044d16f7c5c9b495839f5658c0b3af062
james moran <moranj122@gmail.com>
2021-06-18 09:36:43 +0100

    moai seeds

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -18,7 +18,7 @@ const buildMap = (mapElement) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 120, maxZoom: 15, minZoom: 0 });
+  map.fitBounds(bounds, { padding: 135, maxZoom: 15, minZoom: 0 });
 };
 
 const initMapbox = () => {
